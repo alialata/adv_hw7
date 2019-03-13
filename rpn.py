059487c84138f3823a57acc1e8c4f8749643c0fc
Ali Alata <alialata@umich.edu>
2019-03-13 01:22:41 -0400

    update

--- rpn.py
+++ rpn.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import operator
-
+import readline
+import random
 
 operators = {
     '+': operator.add,
@@ -11,21 +12,24 @@ operators = {
 }
 
 def calculate(myarg):
-    stack = list()
-    for token in myarg.split():
-        try:
-            token = int(token)
-            stack.append(token)
-        except ValueError:
-            function = operators[token]
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
-            stack.append(result)
-        print(stack)
-    if len(stack) != 1:
-        raise TypeError("Too many parameters")
-    return stack.pop()
+	stack = list()
+	for token in myarg.split():
+		try:
+			token = int(token)
+			stack.append(token)
+		except ValueError:
+			if(token == 'r'):
+				stack.append(random.randint(0,100))
+			else:
+				function = operators[token]
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = function(arg1, arg2)
+				stack.append(result)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Too many parameters")
+	return stack.pop()
 
 def ali():
     print("Ali Alata")
